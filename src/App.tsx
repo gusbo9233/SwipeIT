b68db76b777090aaf7b58f08f95594d32ad8362f
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-30 09:44:21 +0200

    add search page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Header from './components/Header'
+import Header from './components/base/Header'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from './pages/NotFound'
+import Search from './pages/Search'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/search" element={<Search />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
