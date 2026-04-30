da3223a5ead9d7b3e050d78817c9d6664aa6b1e6
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-30 15:54:43 +0200

    add empty swipe component

---
+++ src/components/search/SwipeCandidate.tsx
@@ -0,0 +1,12 @@
+function SwipeCandidates() {
+
+    // get the filtered candidates:
+    
+    return ( 
+        <section>
+            empty swipe candidate view
+        </section>
+     );
+}
+
+export default SwipeCandidates;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,25 +1,20 @@
 import './Search.css'
 import SearchView from '../components/search/SearchView'
 import { SearchProvider } from '../context/SearchProvider';
+import SwipeCandidates from '../components/search/SwipeCandidate';
 
 function Search() {
   function onStartSwiping() {
     // slide to secetion for SwipeCandidates.tsx
   }
 
-  const searchProps = { onStartSwiping};
+  const searchProps = {onStartSwiping};
 
   return (
     <SearchProvider>
       <div className="search-page page">
-
-      <section>
         <SearchView {...searchProps}/>
-      </section>
-
-      <section className="swipe-candidates">
-        
-      </section>
+        <SwipeCandidates />
       </div>
     </SearchProvider>
   )
